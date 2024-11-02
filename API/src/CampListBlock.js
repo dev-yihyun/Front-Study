576782299d8151de7a298a8a623337f77b9dccfa
dev-yihyun <dev_yihyun@naver.com>
2024-11-02 20:11:11 +0900

    [feat] 페이지네이션 구현

--- API/src/CampListBlock.js
+++ API/src/CampListBlock.js
@@ -1,9 +1,6 @@
 import "./index.css";
 
 function CampListBlock({ campgroundInfo }) {
-    // 구조분해할당 campgroundInfo를 배열로 받지 못해서 map을 호출 할 때 오류가 발생한다.
-    // 컴포넌트 함수의 매개변수로 campgroundInfo를 받을때 props로 전달된 전체 객체를 받게된다.
-    // 그래서 구조분해할당을 통해 배열로 받아 접근해야한다.
     return (
         <>
             <ol>
@@ -20,3 +17,6 @@ function CampListBlock({ campgroundInfo }) {
 }
 
 export default CampListBlock;
+// 구조분해할당 campgroundInfo를 배열로 받지 못해서 map을 호출 할 때 오류가 발생한다.
+// 컴포넌트 함수의 매개변수로 campgroundInfo를 받을때 props로 전달된 전체 객체를 받게된다.
+// 그래서 구조분해할당을 통해 배열로 받아 접근해야한다.
--- API/src/Main.js
+++ API/src/Main.js
@@ -1,16 +1,25 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import "./App.css";
 import CampListBlock from "./CampListBlock";
 
 function MainBlock() {
+    const [pageNo, setPageNo] = useState(1); //현재 페이지 수
     const [campgroundInfo, setCampGroundInfo] = useState();
-    const API_URL = `https://apis.data.go.kr/B551011/GoCamping/basedList?numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=WebTest&serviceKey=${process.env.REACT_APP_API_KEY}&_type=JSON`;
 
+    const [totalData, setTotalData] = useState(0);
+    const [pageRange, setPageRange] = useState(5);
+    const [buttonRange, setButtonRange] = useState(7);
+
+    const [current, setCurrent] = useState(Math.ceil(pageNo / buttonRange));
+    const [startPage, setStartPage] = useState((current - 1) * buttonRange + 1);
+
+    const API_URL = `https://apis.data.go.kr/B551011/GoCamping/basedList?numOfRows=10&pageNo=${pageNo}&MobileOS=ETC&MobileApp=WebTest&serviceKey=${process.env.REACT_APP_API_KEY}&_type=JSON`;
     const fetchData = async () => {
         try {
             const response = await axios.get(API_URL);
-            setCampGroundInfo(response.data.response.body.items.item);
+            setCampGroundInfo(response?.data?.response?.body?.items?.item);
+            setTotalData(response?.data?.response?.body?.totalCount);
             console.log("response", response);
         } catch (err) {
             console.log(err.message);
@@ -18,12 +27,32 @@ function MainBlock() {
             console.log("Loading..");
         }
     };
+
     useEffect(() => {
         fetchData();
+        setPageNo(1);
+        setPageRange(5);
+        setButtonRange(5);
+        setStartPage((current - 1) * buttonRange + 1);
     }, []);
+    useLayoutEffect(() => {
+        fetchData();
+    }, [pageNo]);
+    useEffect(() => {
+        setStartPage((current - 1) * buttonRange + 1);
+    }, [current, buttonRange, startPage]);
     return (
         <>
             <CampListBlock campgroundInfo={campgroundInfo} />
+            {buttonRange && startPage
+                ? Array(buttonRange)
+                      .fill(startPage)
+                      .map((_, index) => (
+                          <button key={index} onClick={() => setPageNo(startPage + index)}>
+                              {startPage + index}
+                          </button>
+                      ))
+                : null}
         </>
     );
 }
@@ -39,4 +68,28 @@ pageNo : 페이지 번호
 MobileOS : OS 구분
 MobileApp : 서비스명
 serviceKey : 인증키
+*/
+/*    const nextGo = () => {
+        setPageNo((prev) => prev + 1);
+    };
+    const prevGo = () => {
+        if (pageNo <= 0) {
+            return;
+        }
+        setPageNo((prev) => prev - 1);
+    };
+
+    const goToPage = (page) => {
+        setPageNo(page);
+    };
+<button onClick={nextGo}>+</button>
+            <button onClick={prevGo}>-</button>
+
+            <div>
+                {campgroundInfo.map((_, index) => (
+                    <button key={index} onClick={() => goToPage(index + 1)}>
+                        {index + 1}
+                    </button>
+                ))}
+            </div>
 */
