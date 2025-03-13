9f49c001f4fd331f0411da779d86f7135d162979
dev-yihyun <dev_yihyun@naver.com>
2025-03-13 20:29:57 +0900

    next 13

--- Next/note/src/app/contact/page.tsx
+++ Next/note/src/app/contact/page.tsx
@@ -1,3 +1,3 @@
 export default function contact() {
-    return <h1>contact Us</h1>;
+    return <h1>contact page Us</h1>;
 }
---
+++ Next/note/src/app/products/page.tsx
@@ -0,0 +1,3 @@
+export default function ProductsPage() {
+    return <h1>ProductsPage 제품 소개 페이지</h1>;
+}
