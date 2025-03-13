8346831d780b3e184585ab52623cda8bf931fc8d
dev-yihyun <dev_yihyun@naver.com>
2025-03-13 23:49:49 +0900

    next 13 dynamic Routing

---
+++ Next/note/src/app/products/[slug]/page.tsx
@@ -0,0 +1,17 @@
+type Props = {
+    params: {
+        slug: string; //폴더명과 동일하기
+    };
+};
+
+export default function PantsPage({ params }: Props) {
+    return <h1>{params.slug} 제품 설명 페이지</h1>;
+}
+//
+/*
+url에 한글을 입력하면 %%가 나오는 이유?
+URL에서 한글이 깨져 보이는 것은 URL 인코딩된 형태로 전달되어 파싱되었기 때문입니다.
+참고로 URL 인코딩은 비 ASCII 문자를 URL 안에서 안전하게 전송하기 위해 해당 문자를 퍼센트 인코딩된 문자열로 변환하는 과정입니다.
+
+https://nextjs.org/docs/app/building-your-application/routing/dynamic-routes
+*/
