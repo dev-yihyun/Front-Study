273aef06100125b28ee24e1276e80399a3b29c14
dev-yihyun <dev_yihyun@naver.com>
2025-03-15 00:25:37 +0900

    dynamic routing

--- Next/note/src/app/products/[slug]/page.tsx
+++ Next/note/src/app/products/[slug]/page.tsx
@@ -1,12 +1,19 @@
-export default async function PantsPage({ params }: { params: Promise<{ slug: string }> }) {
-    const { slug } = await params;
-    return <h1>{slug} 제품 설명 페이지</h1>;
+export default async function PantsPage({ params }: { params: { slug: string } }) {
+    return <h1>{params.slug} 제품 설명 페이지</h1>;
 }
-//
-/*
-url에 한글을 입력하면 %%가 나오는 이유?
-URL에서 한글이 깨져 보이는 것은 URL 인코딩된 형태로 전달되어 파싱되었기 때문입니다.
-참고로 URL 인코딩은 비 ASCII 문자를 URL 안에서 안전하게 전송하기 위해 해당 문자를 퍼센트 인코딩된 문자열로 변환하는 과정입니다.
 
-https://nextjs.org/docs/app/building-your-application/routing/dynamic-routes
+// 이 함수 이름은 next 규칙이다.
+/*
+Dynamic Route page 에서 특정한 경로에 한에서는 미리 페이지를 만들어 두고 싶다면 
+어떤 경로에 대해서 만들고싶은지 경로를 알려주면 된다.
 */
+export function generateStaticParams() {
+    //내가 미리 렌더링하고싶은 것
+    const products = ["pants", "skirt"];
+    // 배열 안에 잇는 product에 대해서 동일하게 prop 형태로 객체를 전달
+    // params에 들어가는 형태로 객체를 만들어 주면 된다.
+    // 총2개의 slug 객체가 배열로 들어간다.
+    return products.map((product) => ({
+        slug: product,
+    }));
+}
