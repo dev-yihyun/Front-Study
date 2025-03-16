24ffabaf23fb3fe029897f228f25000cfa8aa324
dev-yihyun <dev_yihyun@naver.com>
2025-03-16 17:19:47 +0900

    next12 버전 변경

--- Next/note/src/app/products/[slug]/page.tsx
+++ Next/note/src/app/products/[slug]/page.tsx
@@ -1,4 +1,10 @@
-export default async function PantsPage({ params }: { params: { slug: string } }) {
+type Props = {
+    params: {
+        slug: string; //폴더명과 동일하기
+    };
+};
+
+export default function PantsPage({ params }: Props) {
     return <h1>{params.slug} 제품 설명 페이지</h1>;
 }
 
