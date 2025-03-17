ddb5a5960254280e906b0ea48d27a584c876d57a
dev-yihyun <dev_yihyun@naver.com>
2025-03-18 00:04:13 +0900

    [revert]

--- Next/note/src/app/products/layout.tsx
+++ Next/note/src/app/products/layout.tsx
@@ -1,12 +1,14 @@
-import Link from "next/link";
 import styles from "./layout.module.css";
 
 export default function ProductsLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
+            {/* products 경로 안에 있는 layout으로
+        products경로는 app 폴더 안에 있기 때문에 중첩되어 나온다.
+        */}
             <nav className={styles.nav}>
-                <Link href="/products/women">여성옷</Link>
-                <Link href="/products/man">남성옷</Link>
+                <a>여성옷</a>
+                <a>남성옷</a>
             </nav>
             <section className={styles.product}>{children}</section>
         </>
