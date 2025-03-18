fdd67234c0c5d0036ed05efdd207319e86cd8206
dev-yihyun <dev_yihyun@naver.com>
2025-03-18 15:23:08 +0900

    서버 컴포넌트

--- Next/note/src/app/page.tsx
+++ Next/note/src/app/page.tsx
@@ -1,3 +1,9 @@
+import os from "os";
+import { useState } from "react";
+
 export default function Home() {
+    console.log("test");
+    console.log(os.hostname());
+    const [name, setName] = useState("");
     return <h1>홈페이지다~!</h1>;
 }
