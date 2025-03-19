ebafa1242abd1e206435b10d3c49a823909abe8e
dev-yihyun <dev_yihyun@naver.com>
2025-03-19 19:07:19 +0900

    error ui

--- Next/note/src/app/products/page.tsx
+++ Next/note/src/app/products/page.tsx
@@ -2,15 +2,8 @@ import MeowArticle from "@/components/MeowAritcle";
 import { getProducts } from "@/service/products";
 import Link from "next/link";
 
-// export const revalidate = 3;
-// false | 'force-cache' | 0 | number
-// false(기본값) : SSG로 동작하는 것을 볼 수 있다.
-// 0 : SSR처럼 요청이 올때마다 항상 만들어진다.
-// number : 몇초마다 ISR할 것인지
-
 export default async function ProductsPage() {
-    // 네트워크로 데이터를 읽어와 SSG
-
+    throw new Error("강제 에러 발생!"); // 일부러 에러 발생
     const products = await getProducts();
 
     return (
