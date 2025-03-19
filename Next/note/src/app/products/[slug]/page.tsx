5f93c0e4896399a25fb6908831e0075834fb45aa
dev-yihyun <dev_yihyun@naver.com>
2025-03-19 19:53:15 +0900

    상세페이지

--- Next/note/src/app/products/[slug]/page.tsx
+++ Next/note/src/app/products/[slug]/page.tsx
@@ -1,10 +1,10 @@
 import { getProduct, getProducts } from "@/service/products";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 
-// export const revalidate = 3;
 type Props = {
     params: {
-        slug: string; //폴더명과 동일하기
+        slug: string;
     };
 };
 
@@ -19,12 +19,20 @@ export default async function ProductPage({ params: { slug } }: Props) {
     if (!product) {
         notFound();
     }
-    // 서버 파일에 있는 데이터 중 해당 제품의 정보를 찾아서 그걸 보여줌
-    return <h1>{product.name} 제품 설명 페이지</h1>;
+    return (
+        <>
+            <h1>{product.name} 제품 설명 페이지</h1>
+            <Image
+                src={`/images/${product.image}`}
+                alt={`${product.image}`}
+                width={400}
+                height={400}
+            />
+        </>
+    );
 }
 
 export async function generateStaticParams() {
-    // 모든 제품의 페이지들을 미리 만들어 둘 수 있게 해둠(SSG)
     const products = await getProducts();
     return products.map((product) => ({
         slug: product.id,
