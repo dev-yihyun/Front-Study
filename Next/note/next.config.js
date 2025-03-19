68d16300b469a8097a8b700f68b3bc01c11fad7e
dev-yihyun <dev_yihyun@naver.com>
2025-03-19 22:07:59 +0900

    Redirect

--- Next/note/next.config.js
+++ Next/note/next.config.js
@@ -11,6 +11,21 @@ const nextConfig = {
             },
         ],
     },
+    async redirects() {
+        // redirect 해줄 배열을 리턴한다.
+        return [
+            {
+                source: "/products/delete_forever", // products/delete_forever 경로에 오면
+                destination: "/products", // /products 경로에 오도록 할 것 이다.
+                permanent: true, // 옮겨갔다.
+            },
+            {
+                source: "/products/delete_temp",
+                destination: "/products",
+                permanent: false, // 일시적으로 페이지가 이동했다면
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
