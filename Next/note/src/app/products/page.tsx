b79532364a88434b44e519fbcad87b6523735da7
dev-yihyun <dev_yihyun@naver.com>
2025-03-19 13:58:36 +0900

    fetch를 사용한 CSR

--- Next/note/src/app/products/page.tsx
+++ Next/note/src/app/products/page.tsx
@@ -1,6 +1,6 @@
+import MeowArticle from "@/components/MeowAritcle";
 import { getProducts } from "@/service/products";
 import Link from "next/link";
-import style from "./page.module.css";
 
 // export const revalidate = 3;
 // false | 'force-cache' | 0 | number
@@ -12,15 +12,7 @@ export default async function ProductsPage() {
     // 네트워크로 데이터를 읽어와 SSG
 
     const products = await getProducts();
-    const res = await fetch("https://meowfacts.herokuapp.com", {
-        next: { revalidate: 3 }, // revalidate: 0 -> SSR , revalidate: 3->ISR
-        // cache: "no-store", // ==={revalidate: 0}
-        // 브라우저 검사 > 네트워크 > 캐시사용 중지
-        // cache되면 SSG로 행동
-        cache: "reload",
-    });
-    const data = await res.json();
-    const factText = data.data[0];
+
     return (
         <>
             <h1>ProductsPage 제품 소개 페이지</h1>
@@ -31,7 +23,7 @@ export default async function ProductsPage() {
                     </li>
                 ))}
             </ul>
-            <article className={style.article}>{factText}</article>
+            <MeowArticle />
         </>
     );
 }
