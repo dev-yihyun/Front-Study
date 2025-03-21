e7a395e55ec7b0133825dbb9f5c4671e25d5ca9f
dev-yihyun <dev_yihyun@naver.com>
2025-03-21 18:35:04 +0900

    [feat] Footer

---
+++ Next/next-blog/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <>
+            <div className="bg-blue-200 text-center p-4 mt-5">
+                <p>해당 사이트는 상업적 목적이 없는 개인 포트폴리오 사이트 입니다.</p>
+            </div>
+        </>
+    );
+}
+
+export default Footer;
