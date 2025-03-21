8aed7710f49005b3c75e1829df314455b220abbf
dev-yihyun <dev_yihyun@naver.com>
2025-03-21 18:34:43 +0900

    [feat] about 페이지

--- Next/next-blog/src/app/about/page.tsx
+++ Next/next-blog/src/app/about/page.tsx
@@ -1,9 +1,61 @@
 import Profile from "@/components/Profile";
+import profileData from "../../../data/profileData.json";
 
 function AboutPage() {
     return (
         <>
             <Profile />
+            <section className="text-center bg-indigo-50 rounded-md mt-10 p-8 flex justify-around flex-wrap gap-10 mb-10">
+                <div className="flex flex-col justify-around gap-10">
+                    <div className="flex flex-col items-center justify-center">
+                        <h1 className="font-bold text-4xl">Who am I?</h1>
+                        <ul className="text-left mt-10">
+                            {profileData.introduce.map((item, index) => (
+                                <li key={index} className="mt-2">
+                                    {item}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div>
+                        <h1 className="font-bold text-4xl">Major</h1>
+                        <p className="mt-4">Computer Science</p>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-10">
+                    <div>
+                        <h1 className="font-bold text-4xl">Career</h1>
+                        <p className="mt-4">{profileData.career}</p>
+                    </div>
+                    <div>
+                        <h1 className="font-bold text-4xl">Skills</h1>
+                        <div className="grid grid-cols-2 gap-4 mt-4">
+                            <div>
+                                <ul>
+                                    {profileData.skill
+                                        .slice(0, Math.ceil(profileData.skill.length / 2))
+                                        .map((item, index) => (
+                                            <li key={index} className="mt-2">
+                                                {item}
+                                            </li>
+                                        ))}
+                                </ul>
+                            </div>
+                            <div>
+                                <ul>
+                                    {profileData.skill
+                                        .slice(Math.ceil(profileData.skill.length / 2))
+                                        .map((item, index) => (
+                                            <li key={index} className="mt-2">
+                                                {item}
+                                            </li>
+                                        ))}
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     );
 }
--- Next/next-blog/src/app/layout.tsx
+++ Next/next-blog/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -28,6 +29,7 @@ export default function RootLayout({
             <body className="flex flex-col w-full max-w-screen-2xl px-20 mx-auto">
                 <Header />
                 <main className="grow">{children}</main>
+                <Footer />
             </body>
         </html>
     );
