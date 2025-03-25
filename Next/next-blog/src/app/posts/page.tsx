ebe9d939a4f827d1cff90e35e8b664187df1a80c
dev-yihyun <dev_yihyun@naver.com>
2025-03-25 12:08:36 +0900

    [feat] Post Page

--- Next/next-blog/src/app/posts/page.tsx
+++ Next/next-blog/src/app/posts/page.tsx
@@ -1,7 +1,34 @@
-function PostsPage() {
+import PostCard from "@/components/PostCard";
+import { getAllData } from "@/service/posts";
+
+async function PostsPage() {
+    const data = await getAllData();
+    const category = [...new Set(data.map((index) => index.category))];
     return (
         <>
-            <h1>PostsPage</h1>
+            <section className="flex flex-row gap-5">
+                <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
+                    {data.map((index) => (
+                        <PostCard data={index} key={index.path} />
+                    ))}
+                </div>
+                <div className="list-none ">
+                    <p
+                        className="text-xl font-bold
+                    border-b-5 border-gray-900/25 pb-3 mb-3
+                    "
+                    >
+                        category
+                    </p>
+                    <div className="flex flex-col gap-2 ">
+                        {category.map((item, index) => (
+                            <li key={index} className="cursor-pointer">
+                                {item}
+                            </li>
+                        ))}
+                    </div>
+                </div>
+            </section>
         </>
     );
 }
--- Next/next-blog/src/service/posts.ts
+++ Next/next-blog/src/service/posts.ts
@@ -1,4 +1,5 @@
 import { promises } from "fs";
+import { readFile } from "fs/promises";
 import path from "path";
 
 export type Data = {
@@ -8,10 +9,6 @@ export type Data = {
     category: string;
     path: string;
     featured: boolean;
-};
-
-export type PostData = {
-    content: string;
 };
 
 export async function getAllData(): Promise<Data[]> {
@@ -26,4 +23,15 @@ export async function getFeaturedPosts(): Promise<Data[]> {
 
 export async function getNonFeaturedPosts(): Promise<Data[]> {
     return getAllData().then((datas) => datas.filter((data) => data.featured));
+}
+
+export async function getPostPath(prop: string): Promise<Data | undefined> {
+    const post = await getAllData();
+    return post.find((data) => data.path === prop);
+}
+
+export async function getPostData(prop: string) {
+    const filePath = path.join(process.cwd(), "data", "posts", `${prop}.md`);
+    const content = await readFile(filePath, "utf-8");
+    return { content };
 }
