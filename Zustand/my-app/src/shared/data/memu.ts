014708b9b93053be94c79144c6a5f3f77a62a566
dev-yihyun <dev_yihyun@naver.com>
2025-10-19 02:41:40 +0900

    feat: 사이드바 메뉴 수정

--- Zustand/my-app/src/shared/data/memu.ts
+++ Zustand/my-app/src/shared/data/memu.ts
@@ -1,4 +1,4 @@
-import { Home, LogIn, UserPlus } from "lucide-react";
+import { Home, LogIn, UserPlus, UserSearch } from "lucide-react";
 
 // Menu items.
 export const menuItems = [
@@ -17,10 +17,16 @@ export const menuItems = [
         url: "/signup",
         icon: UserPlus,
     },
+    {
+        title: "Find Account",
+        url: "/find-account",
+        icon: UserSearch,
+    },
 ];
 export const titleMap: Record<string, string> = {
     "/": "Home",
     "/login": "Login",
     "/signup": "Signup",
+    "/find-account": "Find Account",
     "/todo": "Todo",
 };
