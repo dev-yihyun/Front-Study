fe5442fab6b27beafac89f32f31b6971d1016060
dev-yihyun <dev_yihyun@naver.com>
2025-10-21 16:20:54 +0900

    feat: 준비중 컴포넌트 생성

---
+++ Zustand/my-app/src/components/common/Ready.tsx
@@ -0,0 +1,14 @@
+import { Clover } from "lucide-react";
+import React from "react";
+import PageTitle from "./PageTitle";
+
+function ReadyComponent() {
+    return (
+        <section className="flex flex-col items-center gap-4">
+            <Clover size={500} />
+            <p className="text-4xl font-bold text-gray-600">Guest Mode</p>
+            <PageTitle />
+        </section>
+    );
+}
+export default React.memo(ReadyComponent);
