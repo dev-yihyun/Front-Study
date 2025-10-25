fcb7b85f7a9cc4ea73ebc427d34880c5dcf5d159
dev-yihyun <dev_yihyun@naver.com>
2025-10-25 20:32:36 +0900

    chore: 불필요한 콘솔로그 삭제

--- Zustand/my-app/src/shared/hooks/useAuth.ts
+++ Zustand/my-app/src/shared/hooks/useAuth.ts
@@ -10,11 +10,9 @@ export const useAuth = () => {
     // 로그인이 필요한 페이지에서 사용
     const requireAuth = () => {
         if (!isAuthenticated) {
-            console.log("##requireAuth");
             router.push("/login");
             return false;
         }
-        console.log("##requireAuth");
         setIsLoading(false);
         return true;
     };
@@ -22,11 +20,9 @@ export const useAuth = () => {
     // 로그인한 사용자가 접근하면 안되는 페이지에서 사용
     const requireGuest = () => {
         if (isAuthenticated) {
-            console.log("##requireGuest");
             router.push("/");
             return false;
         }
-        console.log("##requireGuest_");
         setIsLoading(false);
         return true;
     };
